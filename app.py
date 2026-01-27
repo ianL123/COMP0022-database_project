6ad4c9c542c89a8328b00bc6111c6f36fb707ccb
Yun Zhu <yunzhu0717@gmail.com>
2026-01-27 14:10:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@db:3306/my_project_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
